facd2b9846c8f46e7253a3b0ec0949f7dcc0cde1
gxq <917709989@qq.com>
2026-02-24 18:10:02 +0800

    1.0.0
    BASE

---
+++ xq-cloud/xq-message/src/test/java/com/share/message/MessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.share.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
